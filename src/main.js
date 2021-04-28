a7525a422ac8d6221ea66adb5fb1e4c11b1962ab
broodroosterdev <myth.usa538@gmail.com>
2021-04-28 16:46:17 +0200

    Added graphql client

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import '@/assets/css/index.css'
 import router from './router'
 import ErrorService from './services/ErrorService'
 import vuetify from './plugins/vuetify'
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 Vue.config.errorHandler = (error) => ErrorService.onError(error);
@@ -13,5 +14,6 @@ Vue.config.errorHandler = (error) => ErrorService.onError(error);
 new Vue({
   router,
   vuetify,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
