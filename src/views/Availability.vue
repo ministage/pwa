4775d974bc8a93b19e79265c8603908b3f34fd25
broodroosterdev <myth.usa538@gmail.com>
2021-04-29 11:11:55 +0200

    Let availability page use GraphQL

--- src/views/Availability.vue
+++ src/views/Availability.vue
@@ -7,8 +7,8 @@
 
     <div class="rounded-full h-24 w-24 flex items-center justify-center bg-green-400">Aanwezig</div>
 
-    <div v-if="precenses" class="w-full px-3 grid lg:grid-cols-2 grid-cols-1 gap-4">
-      <div v-for="presence in precenses" :key="presence.id">
+    <div v-if="presences" class="w-full px-3 grid lg:grid-cols-2 grid-cols-1 gap-4">
+      <div v-for="presence in presences" :key="presence.id">
         <table class="outline-black m-2 border-collapse w-11/12">
           <tr class="bg-blue-200 m-10">
             <th>Naam</th>
@@ -17,8 +17,8 @@
           </tr>
           <tr>
             <td>{{ presence.user.username }}</td>
-            <td>{{ presence.is_present }}</td>
-            <td>{{ presence.user.company }}</td>
+            <td>{{ presence.is_present ? "Aanwezig" : "Afwezig" }}</td>
+            <td>{{ presence.user.company.name }}</td>
           </tr>
         </table>
       </div>
@@ -28,46 +28,25 @@
 </template>
 
 <script>
-import ReserveringAPI from '../services/reservering'
-import ErrorService from '../services/ErrorService'
+import gql from 'graphql-tag';
 
 export default {
-  data() {
-    return {
-      loading: false,
-      rooms: null,
-      precenses: null,
-      error: null
-    }
-  },
-  created() {
-    // fetch the data when the view is created and the data is
-    // already being observed
-    this.fetchData()
+  apollo: {
+    presences: gql`query {
+      presences {
+        id
+        user {
+          username
+          company {
+            name
+          }
+        }
+        is_present
+      }
+    }`
   },
   name: 'Home',
-  components: {
-    //RoomCard
-  },
   methods: {
-    fetchData() {
-      this.error = this.rooms = null
-      this.loading = true
-
-      ReserveringAPI.getPresences().then(presences => {
-        this.loading = false;
-        this.precenses = presences;
-      }).catch(error => {
-        ErrorService.displayErrorAlert(error);
-      })
-
-      ReserveringAPI.getRooms().then(rooms => {
-        this.loading = false;
-        this.rooms = rooms;
-      }).catch(error => {
-        ErrorService.displayErrorAlert(error);
-      });
-    },
     gotoReserve() {
       this.$router.push('/reserve');
     }
