0f818277229ce43b20e4916d5c8ed773c33e0f42
Julien2212 <58032666+Julien2212@users.noreply.github.com>
2021-04-29 17:06:11 +0200

    Aanwezigheid kleur ding

---
+++ src/components/PresenceCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card elevation="2">
+    <div class="d-flex flex-no-wrap">
+      <v-avatar size="125" rounded>
+        <v-img :src="transformUrl(company.logo.url)"></v-img>
+      </v-avatar>
+
+      <div>
+        <v-card-title>{{ company.name }} - {{ countPresent(company.employees) }} aanwezig</v-card-title>
+        <v-card-text>{{company.description}}</v-card-text>
+      </div>
+      <v-btn
+          icon
+          class="ml-auto mt-3 mr-3"
+          @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </div>
+    <v-expand-transition>
+      <v-list v-show="show">
+        <v-subheader v-for="employee in company.employees" :key="employee.id" :class="employee.presence.is_present ? 'bg-green-200' : 'bg-red-200'">{{ employee.username }} -
+          {{ employee.presence.is_present ? "Aanwezig" : "Afwezig" }}
+        </v-subheader>
+      </v-list>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+
+<script>
+export default {
+  name: 'PresenceCard',
+  data: () => ({
+    show: false
+  }),
+  props: {
+    company: Object,
+  },
+  methods: {
+    transformUrl: function (url) {
+      return "http://localhost:1337" + url;
+    },
+    countPresent: function (employees) {
+      return employees.filter(employee => employee.presence.is_present).length;
+    }
+  }
+}
+</script>
--- src/views/Availability.vue
+++ src/views/Availability.vue
@@ -1,54 +1,67 @@
 <template>
   <div class="flex flex-col justify-center">
-    <h1 class="text-4xl ml-4">Aanwezigheid</h1>
-    <div v-if="loading" class="loading">
-      Loading...
+    <h1 class="text-4xl">Aanwezigheid</h1>
+    <h2>Welkom naam</h2>
+
+    <!-- zorgen dat het groen/rood is en dat dit aangepast wordt {{1+2 === 3 ? 'bg-green-200' : 'bg-red-200'}} -->
+    <div class="text-left">
+      <v-btn id="button1" class="m-2" fab dark large color="green" @click="changeColor">
+        <v-icon dark>
+          mdi-check
+        </v-icon>
+      </v-btn>
     </div>
-
-    <div class="rounded-full h-24 w-24 flex items-center justify-center bg-green-400">Aanwezig</div>
-
-    <div v-if="presences" class="w-full px-3 grid lg:grid-cols-2 grid-cols-1 gap-4">
-      <div v-for="presence in presences" :key="presence.id">
-        <table class="outline-black m-2 border-collapse w-11/12">
-          <tr class="bg-blue-200 m-10">
-            <th>Naam</th>
-            <th>Aanwezigheid</th>
-            <th>Bedrijf</th>
-          </tr>
-          <tr>
-            <td>{{ presence.user.username }}</td>
-            <td>{{ presence.is_present ? "Aanwezig" : "Afwezig" }}</td>
-            <td>{{ presence.user.company.name }}</td>
-          </tr>
-        </table>
-      </div>
-    </div>
-
+    <h2>Klik hierboven om je afwezig te melden</h2>
+    <v-row dense>
+      <v-col cols="6" v-for="company in companies" :key="company.id">
+        <PresenceCard :company="company"></PresenceCard>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 import gql from 'graphql-tag';
+import PresenceCard from '../components/PresenceCard'
 
 export default {
   apollo: {
-    presences: gql`query {
-      presences {
-        id
-        user {
-          username
-          company {
-            name
-          }
-        }
+    companies: gql`query {
+  companies{
+    id
+    name
+    description
+    logo{
+      url
+    }
+    employees{
+      id
+      username
+      presence{
         is_present
       }
-    }`
+    }
+  }
+}`
   },
   name: 'Home',
+  components: {
+    PresenceCard,
+  },
   methods: {
     gotoReserve() {
       this.$router.push('/reserve');
+    },
+    changeColor(){
+      let count = 0;
+      if (count === 0) {
+        document.getElementById("button1").style.backgroundColor = "red";
+        count = 1;
+      }
+      else if(count === 1) {
+        document.getElementById("button1").style.backgroundColor = "green";
+        count = 0;
+      }
     }
   }
 }
