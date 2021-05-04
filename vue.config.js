637ee3bd2ac439bd10b89c750639d75ed17b0907
broodroosterdev <myth.usa538@gmail.com>
2021-05-04 12:36:33 +0200

    Change icons

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,34 @@
 module.exports = {
-  configureWebpack: {
-    devServer: {
-      headers: { "Access-Control-Allow-Origin": "*" }
+    configureWebpack: {
+        devServer: {
+            headers: {"Access-Control-Allow-Origin": "*"}
+        }
+    },
+
+    pluginOptions: {
+        apollo: {
+            lintGQL: true
+        }
+    },
+
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/pwa/'
+        : '/',
+
+    transpileDependencies: [
+        'vuetify'
+    ],
+
+    pwa: {
+        name: "California",
+        themeColor: "#e0bfbf",
+        iconPaths: {
+            faviconSVG: null,
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/mstile-150x150.png'
+        },
     }
-  },
-
-  pluginOptions: {
-    apollo: {
-      lintGQL: true
-    }
-  },
-
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/pwa/'
-  : '/',
-
-  transpileDependencies: [
-    'vuetify'
-  ]
 };
