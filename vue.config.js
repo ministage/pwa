db7d050a335783f448c3a78a20a940a3f7661425
broodroosterdev <myth.usa538@gmail.com>
2021-05-04 12:44:38 +0200

    Fix typo in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ module.exports = {
         name: "California",
         themeColor: "#e0bfbf",
         iconPaths: {
-            faviconSVG: ,
+            faviconSVG: null,
             favicon32: 'img/icons/favicon-32x32.png',
             favicon16: 'img/icons/favicon-16x16.png',
             appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
