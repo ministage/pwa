96271408dc349efcc3d51d9b39230a870c073ae4
broodroosterdev <myth.usa538@gmail.com>
2021-05-17 15:46:51 +0200

    Add markdown renderer to more page

---
+++ src/components/Markdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <vue-markdown class="pl-2 pt-2 pr-2">{{content}}</vue-markdown>
+</template>
+
+<script>
+import VueMarkdown from 'vue-markdown';
+export default {
+  name: "Markdown",
+  props: {
+    content: String,
+  },
+  components: {VueMarkdown}
+}
+</script>
+
+<style lang="scss">
+h1 {
+  @apply font-semibold text-3xl
+}
+h2 {
+  @apply font-semibold text-2xl
+}
+h3 {
+  @apply font-semibold text-xl
+}
+</style>
--- src/views/More.vue
+++ src/views/More.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="h-full">
     <PageHeader icon="mdi-account-outline" name="Meer"></PageHeader>
-    <v-card class="h-full">
-      <p class="mt-5 ml-5"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
-        felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo,
-        fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
-        justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper
-        nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-        Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius
-        laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-        Nam eget dui. </p>
-    </v-card>
+    <Markdown
+        v-if="!$apollo.queries.content.loading"
+        :content="content"
+    ></Markdown>
+
   </div>
 </template>
 
 <script>
 import PageHeader from "@/components/PageHeader";
-
+import Markdown from "@/components/Markdown";
+import gql from "graphql-tag";
 export default {
   name: "More",
-  components: {PageHeader}
+  apollo: {
+    content: {
+      query: gql`query{
+        meer {
+            content
+        }
+      }`,
+      update(data) {
+        return data.meer.content;
+      },
+      fetchPolicy: 'cache-and-network'
+    },
+  },
+  components: {
+    PageHeader,
+    Markdown
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
