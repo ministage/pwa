8d8edf425e33389048ecd631f68f9d16f51b3bd5
broodroosterdev <myth.usa538@gmail.com>
2021-05-25 10:53:38 +0200

    Remove error handler

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -65,11 +65,6 @@ export function createProvider (options = {}) {
         // fetchPolicy: 'cache-and-network',
       },
     },
-    errorHandler (error) {
-
-      // eslint-disable-next-line no-console
-      console.log('%cError', 'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;', error.message)
-    },
   })
 
   return apolloProvider
