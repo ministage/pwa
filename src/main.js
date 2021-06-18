1341e30edba61160000e2784958c65bf60398571
broodroosterdev <myth.usa538@gmail.com>
2021-06-18 09:50:14 +0200

    Verwijderen van logging services

--- src/main.js
+++ src/main.js
@@ -3,35 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import '@/assets/css/index.css'
 import router from './router'
-//import ErrorService from './services/ErrorService'
-import vuetify from './plugins/vuetify'
 import { createProvider } from './vue-apollo'
 import {Auth, AxiosTransport, Directus, LocalStorage} from '@directus/sdk';
 import {API_URL} from "@/constants/settings";
-import LogRocket from 'logrocket';
-import * as Sentry from "@sentry/vue";
-import { Integrations } from "@sentry/tracing";
-
-LogRocket.init('lfdcpq/californiapp');
-
-Sentry.init({
-  Vue,
-  dsn: "https://5187f989104a4d57bf501251f066210d@o722539.ingest.sentry.io/5781703",
-  integrations: [new Integrations.BrowserTracing({
-    environment: 'production',
-    tracingOptions: {
-      trackComponents: true,
-    },
-    logErrors: true,
-    attachProps: true,
-    attachStacktrace: true,
-  })],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+import vuetify from './plugins/vuetify';
 
 const storage = new LocalStorage();
 // Transport used to communicate with the server.
@@ -55,7 +30,7 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  vuetify,
   apolloProvider: createProvider(),
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
